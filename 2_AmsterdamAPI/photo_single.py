c24d3b3e405d9b90d98160c6f531d646ac1b2e96
Chris Lucas <chris.lucas@GEODAN.NL>
2017-10-05 15:45:21 +0200

    Exercise 1

--- 2_AmsterdamAPI/photo_single.py
+++ 2_AmsterdamAPI/photo_single.py
@@ -5,13 +5,12 @@ import requests
     # request data from the amsterdam panorama API
     # (url: https://api.data.amsterdam.nl/panorama/recente_opnames/2016/)
     # HINT: use the `get` function of the `requests` module
-response = requests.get('https://api.data.amsterdam.nl/panorama/recente_opnames/2016/')
-## create a python dictionary from the response
-    # HINT: use the json method of the response object
-    # You can display the methods available of an object by typing:
-    # dir(*object_name*) for a simple list of functions and attributes or
-    # help(*object_name*) for a list of functions and attributes with
-    # a description (if written by the author of the class)
+response = .....?
+## We are going to create a python dictionary from the response
+# To do this we use the .json method of the response object.
+# json is a general web data sharing format and is structured 
+# the exact same way a Python dictionary is (probably not by coincidence).
+# Therefore we can use it to create a python dictionary.
 response_dict = response.json()
 
 ## retrieve the image url
@@ -19,21 +18,25 @@ response_dict = response.json()
     # HINT: explore the dictionary using the variable explorer
     # available in spyder.
     # HINT: how do we get a value out of a dictionary?
-url = response_dict['results'][0]['image_sets']['equirectangular']['full']
+url = response_dict['results'].....?
 ## request the image
     # use the image url to get the image
     # HINT: remember how we used the `requests` module to retrieve data from an url?
-image = requests.get(url)
+image_response = .....?
+
+# The response we get through the requests module contains
+# data and functions (like header information and for exmample the .json method we used before)
+# That we do not need now. To get the actual data of the image (in bytes)
+# we specify we want to content like so:
+image = image_response.content
 
 ## save the image to the hard disk
     # save the image to the data/ directory
     # use the filename as defined in the response dictionary
+filename = response_dict['results'].......?
     # HINT: remember how we wrote data to a file?
-    # HINT: to write bytes to a file instead of text, use 'wb' instead of just 'w'
+    # To write bytes to a file instead of text, use 'wb' instead of just 'w'
     # when opening the file.
-    # HINT: use dir() or help() to list the methods and attributes of the
-    # image response. which method or attribute could give the image in a
-    # format we can write to a file?
-filename = 'data\\' + response_dict['results'][0]['filename']
-with open(filename, 'wb') as f:
-    f.write(image.content)
+output_file = open(filename, 'wb')
+output_file.write(......?)
+output_file.close()
